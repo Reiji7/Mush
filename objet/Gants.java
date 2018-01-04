f51a42dc7d452dc32e9ee0d259f9b18b3201ba99
Reiji7 <adrien.claudel.info@gmail.com>
2018-01-04 01:21:26 +0100

    Commentaire de l'objet gants de protection

--- objet/Gants.java
+++ objet/Gants.java
@@ -2,15 +2,28 @@ package objet;
 
 import config.ListObjets;
 
+
+/**
+ * Objet gants 
+ * @author adrien
+ */
 public class Gants extends Objet{
 	
+	
 	public Gants() {
+		
+		// Définit le nom de l'objet 
+		// a prendre dans la liste des objets sur le PDF
 		this.setNom("Paire de gants de protection");
+		
+		// Définit l'id de l'objet
+		// aller chercher dans l'enum: ListObjets dans le package config
 		this.setId(ListObjets.GANTS.getID());
 	}
 	
 	@Override
 	public String toString() {
+		// Compléter la description avec la description du PDF
 		return this.getNom() + ": Doivent  être  dans  l’inventaire  pendant  une réparation pour ne pas se salir";
 	}
 }
