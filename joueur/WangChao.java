99db13dc71600a9edfa43ff7556e4fc9022804bf
Reiji7 <adrien.claudel.info@gmail.com>
2018-01-05 16:38:00 +0100

    Ajout d'objet test dans l'inventaire de WangChao

--- joueur/WangChao.java
+++ joueur/WangChao.java
@@ -1,9 +1,15 @@
 package joueur;
 
+import objet.*;
+
 public class WangChao extends Joueur{
 
 	public WangChao() {
 		this.nom = "Wang Chao";
+		ajout(new Molette());
+		ajout(new Gants());
+
 	}
+	
 	
 }
