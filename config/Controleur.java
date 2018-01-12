e9c43fdf24837413d8d1bbe50c3dfb2028bbebe3
Reiji7 <adrien.claudel.info@gmail.com>
2018-01-12 23:04:23 +0100

    Mise en place de la boucle principal

--- config/Controleur.java
+++ config/Controleur.java
@@ -2,11 +2,13 @@ package config;
 
 import java.util.ArrayList;
 import joueur.Joueur;
+import vaisseau.Salle;
 
 
 public class Controleur {
 	
 	private static ArrayList<Joueur> Joueurs = Initialisation.initJoueur();
+	private static ArrayList<Salle> Salles = Initialisation.initVaisseau();
 	
 	public static void lancement() {
 		for(Joueur j : Joueurs) {
