003438b134e0bd438e6fc5664e4d0820ad9a9b21
Reiji7 <adrien.claudel.info@gmail.com>
2018-01-12 22:44:14 +0100

    Ajout de deux actions

---
+++ joueur/actions/AffichageComm.java
@@ -0,0 +1,10 @@
+package joueur.actions;
+
+import config.Static;
+
+public class AffichageComm extends Action{
+
+	public void affichage() {
+		Static.talkyWalky.affichage();
+	}
+}
---
+++ joueur/actions/AffichageTele.java
@@ -0,0 +1,11 @@
+package joueur.actions;
+
+import config.Static;
+
+public class AffichageTele extends Action{
+
+	public void affichage() {
+		Static.telepathie.affichage();
+	}
+	
+}
